0bb1ee024866d0f0a7dddab08796a83c37d77e8d
Hristo Erinin <zorlem@gmail.com>
2013-05-16 04:41:39 +0200

    Added statistics to split-mysql.pl
     * Print the number of lines inserted into each new file
     * Print elapsed time

--- mysql/split-mysqldump.pl
+++ mysql/split-mysqldump.pl
@@ -9,7 +9,7 @@ use warnings;
 use strict;
 use autodie;
 use English qw( -no_match_vars);
-use Time::HiRes;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 $OUTPUT_AUTOFLUSH=1;
 
@@ -22,6 +22,7 @@ Usage: $0 [-d] <input.sql>
 USAGE
 }
 
+my $starttime=[gettimeofday];
 my $database;
 my $linecount=0;
 my $dbcount=0;
@@ -30,11 +31,11 @@ open my $outfh, '>', "$outfile.sql";
 
 while(<>) {
   if (m|\A-- Current Database: `(.+)`\Z|) {
+    say "lines: $linecount, elapsed: " . tv_interval($starttime) . " sec";
     $database=$1;
     $linecount=0;
     $dbcount++;
-    my $starttime=time;
-    say "database: $database";
+    print "Splitting database: $database, ";
     # encountered a new database, time to switch the files
     close $outfh;
     $outfile=sprintf("%.5d", $dbcount) . q{-} . $database;
@@ -43,5 +44,5 @@ while(<>) {
   $linecount++;
   print $outfh $_;
 }
-
+say "lines: $linecount, elapsed: " . tv_interval($starttime) . " sec";
 close $outfh;
